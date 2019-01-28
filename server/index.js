7c834082598568a9f7d8309100e79240ff2fb2eb
hwgrell <hwgrell@gmail.com>
2019-01-28 10:31:11 -0700

    Developing the application server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+
+app.get('/temperature', function(req, res) {res.send ('24 C'});
+app.get('/humidity', function(req, res) {res.send('48%'});
+
+app.listen(3000, funtion() {
+	console.log(Server listening on port 3000);
+	});
