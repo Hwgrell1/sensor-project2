f7e8cee169458a1fdecebdb7dce5ac4bc9b3a7a5
hwgrell <hwgrell@gmail.com>
2019-01-31 08:56:46 -0700

    DH T11 sensor reading

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-  const express = require('express');    const app = express();
-    app.get('/temperature', function(req, res) {      res.send('24 °C');    });
-    app.get('/humidity', function(req, res) {     res.send('48%');    });
-    app.listen(3000, function(){     console.log('Server listening on port 3000');    }); 
+const express = require('express');
+const app = express();
+
+app.get('/temperature', function(req, res) {res.send('24 °C');});
+app.get('/humidity', function(req, res) {res.send('48%');});
+
+app.listen(3000, function(){console.log('Server listening on port 3000');    }); 
