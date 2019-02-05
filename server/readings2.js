419c271dcac9360d502f5d6d51f101f23406e371
hwgrell <hwgrell@gmail.com>
2019-02-05 06:22:43 -0700

    getSensorReadings module

--- server/readings2.js
+++ server/readings2.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express() 
-const getSensorReadings = require('getSensorReadings')
+const getSensorReadings = require('.get_sensor-readings')
 
 app.get('/temperature', function (req, res) {
 	getSensorReadings((err, temperature, humidity) => {
