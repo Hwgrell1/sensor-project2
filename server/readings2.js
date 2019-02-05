f0d00dd66b14189371c66f21a6fafbb5a14478b0
Harold Grell <hwgrell@gmail.com>
2019-02-05 06:34:51 -0700

    Reverse

--- server/readings2.js
+++ server/readings2.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express() 
-const getSensorReadings = require('.get_sensor-readings')
+const getSensorReadings = require('./get_sensor-readings')
 
 app.get('/temperature', function (req, res) {
 	getSensorReadings((err, temperature, humidity) => {
