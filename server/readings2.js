4bc561ae6a10d2fad0d4967f01278bd4db8b68b0
hwgrell <hwgrell@gmail.com>
2019-02-05 15:12:46 -0700

    Using cache memory

--- server/readings2.js
+++ server/readings2.js
@@ -4,11 +4,11 @@ const gCSR = require('./cache')
 
 
 app.get('/temperature', function (req, res) {
-	res.send(gCSR.getTemperature.toFixed(1) + '°C') 
+	res.send(gCSR.getTemperature .toFixed(1) + '°C') 
 	}) 
 
 app.get('/humidity', function (req, res) {
-	res.send(gCSR.getHumidity.toFixed(1) + '%') 
+	res.send(gCSR.getHumidity .toFixed(1) + '%') 
 	} ) 
 app.listen(3000, function () { console.log('Server listening on port 3000') 
 	})
